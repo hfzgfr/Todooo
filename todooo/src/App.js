68193fcad7373ba12138e5219e2d5e2586be06b9
hfzgfr <hfzgfr@gmail.com>
2021-02-06 09:28:48 +0800

    User can create their own task

--- todooo/src/App.js
+++ todooo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TodayList from "./Components/TodayList";
+import CreateTaskForm from "./Components/CreateTaskForm";
 
 class App extends Component {
   state = {
@@ -18,14 +19,26 @@ class App extends Component {
       },
     ],
   }
+  addTask = (title, details) => {
+    let newTask = { title: title, details: details };
+    let tasks = this.state.tasks;
+    tasks.push(newTask);
+    this.setState({ tasks: tasks });
+  };
   render() {
     // let task_list = this.state.tasks.map(task => (
     //   <p key={task.title}>
     //     {task.title} - {task.details}
     //   </p>
     // ));
-    return <div className="App"><TodayList tasks={this.state.tasks} /></div>;
+    return (
+      <div className="App">
+        <CreateTaskForm addTask={this.addTask}/>
+        <TodayList tasks={this.state.tasks} />
+      </div>
+    );
   }
+      
 }
 
 export default App;
